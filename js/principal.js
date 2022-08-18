2d8dced90d8f13c028ea980bb96a64a3d5d8f81e
Daniel Caresma Silva <91802662+DanielCaresma@users.noreply.github.com>
2022-08-18 13:00:51 -0300

    Calculando IMC do primeiro paciente

--- js/principal.js
+++ js/principal.js
@@ -1,3 +1,16 @@
 var titulo = document.querySelector(".titulo");
-
 titulo.textContent = "Aparecida Nutricionista";
+
+
+var paciente = document.querySelector("#primeiro-paciente");
+
+var tdpeso = paciente.querySelector(".info-peso");
+var peso = tdpeso.textContent;
+
+var tdaltura = paciente.querySelector(".info-altura");
+var altura = tdaltura.textContent;
+
+var tdImc = paciente.querySelector(".info-imc");
+var imc = peso / (altura * altura);
+
+tdImc.textContent = imc;
