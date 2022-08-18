841a6c6c83552125f65b53edd301dd90ba9a6ead
Daniel Caresma Silva <91802662+DanielCaresma@users.noreply.github.com>
2022-08-18 13:58:36 -0300

    Validando informação de poso e altura

--- js/principal.js
+++ js/principal.js
@@ -11,6 +11,24 @@ var tdaltura = paciente.querySelector(".info-altura");
 var altura = tdaltura.textContent;
 
 var tdImc = paciente.querySelector(".info-imc");
-var imc = peso / (altura * altura);
 
-tdImc.textContent = imc;
+
+var pesoEhValido = true;
+var alturaEhValida = true;
+
+if (peso <= 0 || peso >= 300) {
+    console.log("Peso inválido!");
+    pesoEhValido = false;
+    tdImc.textContent = "Peso inválido!"
+}
+
+if (altura <= 0 || altura >= 3.00) {
+    console.log("Altura inválida!");
+    alturaEhValida = false;
+    tdImc.textContent = "Altura inválida!"
+}
+
+if (pesoEhValido && alturaEhValida) {
+    var imc = peso / (altura * altura);
+    tdImc.textContent = imc;
+}
