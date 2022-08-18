04bed52ea8ba8bf5c7cebaf31b660ab343817eb9
Daniel Caresma Silva <91802662+DanielCaresma@users.noreply.github.com>
2022-08-18 14:14:40 -0300

    Calculando o IMC de todos os pacientes

--- js/principal.js
+++ js/principal.js
@@ -2,33 +2,38 @@ var titulo = document.querySelector(".titulo");
 titulo.textContent = "Aparecida Nutricionista";
 
 
-var paciente = document.querySelector("#primeiro-paciente");
+var pacientes = document.querySelectorAll(".paciente");
 
-var tdpeso = paciente.querySelector(".info-peso");
-var peso = tdpeso.textContent;
+for (var i = 0; i < pacientes.length; i++) {
 
-var tdaltura = paciente.querySelector(".info-altura");
-var altura = tdaltura.textContent;
+    var paciente = pacientes[i];
 
-var tdImc = paciente.querySelector(".info-imc");
+    var tdpeso = paciente.querySelector(".info-peso");
+    var peso = tdpeso.textContent;
+
+    var tdaltura = paciente.querySelector(".info-altura");
+    var altura = tdaltura.textContent;
+
+    var tdImc = paciente.querySelector(".info-imc");
 
 
-var pesoEhValido = true;
-var alturaEhValida = true;
+    var pesoEhValido = true;
+    var alturaEhValida = true;
 
-if (peso <= 0 || peso >= 300) {
-    console.log("Peso inválido!");
-    pesoEhValido = false;
-    tdImc.textContent = "Peso inválido!"
-}
+    if (peso <= 0 || peso >= 300) {
+        console.log("Peso inválido!");
+        pesoEhValido = false;
+        tdImc.textContent = "Peso inválido!"
+    }
 
-if (altura <= 0 || altura >= 3.00) {
-    console.log("Altura inválida!");
-    alturaEhValida = false;
-    tdImc.textContent = "Altura inválida!"
-}
+    if (altura <= 0 || altura >= 3.00) {
+        console.log("Altura inválida!");
+        alturaEhValida = false;
+        tdImc.textContent = "Altura inválida!"
+    }
 
-if (pesoEhValido && alturaEhValida) {
-    var imc = peso / (altura * altura);
-    tdImc.textContent = imc;
+    if (pesoEhValido && alturaEhValida) {
+        var imc = peso / (altura * altura);
+        tdImc.textContent = imc.toFixed(2);
+    }
 }
